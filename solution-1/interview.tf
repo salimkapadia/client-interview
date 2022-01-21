4f3dc2f036a295f79c5cc48ea556dcee542211af
Salim Kapadia <salim.kapadia@alcon365.com>
2022-01-21 15:00:30 -0600

    fixed validate issue

--- solution-1/interview.tf
+++ solution-1/interview.tf
@@ -1,32 +1,34 @@
 terraform {
-  backend "s3" {}
+  # backend "s3" {}
 }
 
 provider "aws" {}
 
+
 resource "aws_s3_bucket" "s3" {
-  bucket = "$(var.repository_name)-${terraform.workspace}-Bucket"
+  bucket = "${var.repository_name}-${terraform.workspace}-Bucket"
   tags   = local.tags
-  acl    = "public"
+  acl    = "private"
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "aws:kms"
       }
     }
-  }  
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "s3Access" {
-  bucket = s3.id
+  bucket = aws_s3_bucket.s3.id
 
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_metric" "s3_metrics" {
+  bucket = aws_s3_bucket.s3.id
   name   = "EntireBucket"
 }
 
@@ -38,7 +40,7 @@ resource "aws_s3_bucket_policy" "ssl_only_policy" {
 data "aws_iam_policy_document" "bucket_ssl_only_policy_document" {
   // Force SSL-only access
   statement {
-    sid = "ForceSSLOnlyAccess"
+    sid    = "ForceSSLOnlyAccess"
     effect = "Allow"
     principals {
       type        = "AWS"
---
+++ solution-1/variables.tf
@@ -0,0 +1,5 @@
+variable "repository_name" {
+  default     = "REALLY-COOLO-S3-BUCKET-NAME"
+  description = "This will be the name that is prefixed with the workspace in question"
+  type        = string
+}
