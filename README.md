037e226f6040d2ace89f2f85236020e5a33f2a9f
Salim Kapadia <salim.kapadia@alcon365.com>
2022-01-21 14:46:42 -0600

    added original terraform file received

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # client-interview
+
+I was asked to get `terraform-validate` to work but that seemed to easy. So I figured i'll go above and beyond :-)
---
+++ original/interview.tf
@@ -0,0 +1,60 @@
+terraform {
+  backend "s3" {}
+}
+
+provider "aws" {}
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "$(var.repository_name)-${terraform.workspace}-Bucket"
+  tags   = local.tags
+  acl    = "public"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }  
+}
+
+resource "aws_s3_bucket_public_access_block" "s3Access" {
+  bucket = s3.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_metric" "s3_metrics" {
+  name   = "EntireBucket"
+}
+
+resource "aws_s3_bucket_policy" "ssl_only_policy" {
+  bucket = aws_s3_bucket.s3.id
+  policy = data.aws_iam_policy_document.bucket_ssl_only_policy_document.json
+}
+
+data "aws_iam_policy_document" "bucket_ssl_only_policy_document" {
+  // Force SSL-only access
+  statement {
+    sid = "ForceSSLOnlyAccess"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.s3.arn,
+      "${aws_s3_bucket.s3.arn}/*",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["true"]
+    }
+  }
+}
